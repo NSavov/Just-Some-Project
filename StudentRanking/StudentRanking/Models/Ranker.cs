b97130c30760b1abdd2324103658bf418933f1a7
NSavov <nedko066@gmail.com>
2015-06-18 19:13:27 +0300

    Ranker implementation checkpoint 1

--- StudentRanking/StudentRanking/Models/Ranker.cs
+++ StudentRanking/StudentRanking/Models/Ranker.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -29,12 +30,12 @@ namespace StudentRanking.Models
         }
 
         //Returns a list of preferences of a student by SSN
-        private List<Preference> getStudentPreferences(Student student)
+        private List<Preference> getStudentPreferences(String EGN)
         {
             List<Preference> preferences = new List<Preference>();
 
             var query = from pref in context.Preferences
-                        where pref.EGN == student.EGN
+                        where pref.EGN == EGN
                         select pref;
 
             foreach (var pref in query)
@@ -62,9 +63,80 @@ namespace StudentRanking.Models
             return students;
         }
 
+        private String getFaculty(String programmeName)
+        {
+            var query = from ProgrammeToFaculty in context.Faculties
+                        where ProgrammeToFaculty.ProgrammeName == programmeName
+                        select ProgrammeToFaculty.FacultyName;
+
+            return query.First();
+        }
+
+        private Dictionary<String, List<Preference>> splitPreferencesByFaculty(List<Preference> preferences)
+        { 
+            Dictionary<String, List<Preference>> splittedPreferences = new Dictionary<String, List<Preference>>();
+            List<Preference> value;
+
+            foreach(Preference preference in preferences)
+            {
+                String faculty = getFaculty(preference.ProgrammeName);
+                
+
+                if (!splittedPreferences.TryGetValue(faculty, out value))
+                {
+                    splittedPreferences.Add(faculty, new List<Preference>());
+                }
+
+                splittedPreferences[faculty].Add(preference);
+            }
+
+            return splittedPreferences;
+        }
+
+        public void start()
+        {
+            List<Student> students = new List<Student>();
+
+            //getting all unenrolled students
+            var getStudentsQuery = from student in context.Students
+                                   where student.IsEnrolled == false
+                                   select student;
+
+            foreach (var student in getStudentsQuery)
+            {
+                students.Add(student);
+            }
+
+            for(int i=0; i<students.Count; i++)
+            {
+                //handle preferences
+                List<Preference> allPreferences = getStudentPreferences(students[i].EGN);
+                Dictionary<String, List<Preference>> preferences = splitPreferencesByFaculty(allPreferences);
+                
+                //handle grading
+
+            }
+        }
+
         public void test()
         {
-            
+            //Preference pref1 = new Preference(){EGN = "1234121", IsApproved = false, PrefNumber = 1, ProgrammeName = "CS"};
+            //Preference pref2 = new Preference() { EGN = "1234121", IsApproved = false, PrefNumber = 2, ProgrammeName = "ST" };
+            //Student s = new Student() {EGN="123445", Email="asdf", FirstName="A", Gender=true, IsEnrolled = false, LastName = "L" };
+            //Student s2 = new Student() { EGN = "123345", Email = "asdaf", FirstName = "A1", Gender = true, IsEnrolled = false, LastName = "L1" };
+            ////context.Students.Add(s);
+            ////context.Students.Add(s2);
+            ////context.SaveChanges();
+            //context.Preferences.Add(pref1);
+            //context.Preferences.Add(pref2);
+            //context.SaveChanges();
+
+            var pref = this.getStudentPreferences("1234121");
+
+            Debug.WriteLine("test");
+
+            foreach(Preference preference in pref)
+            Debug.WriteLine(preference.ProgrammeName);
         }
 
     }
